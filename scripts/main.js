e39a366b4ae22c0bfe88a42dbb16abe94c2614d4
ChronosWithA-K <143678373+ChronosWithA-K@users.noreply.github.com>
2024-04-24 16:39:55 +0000

    finish task 1

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+// Task 1 - Print the first 10 square numbers (1*1 = 1, 2*2 = 4, etc.)
+
+console.log("Task 1");
+
+for (i = 1; i < 11; i * i) {
+    console.log(i);
+}
+
+// Task 2 - Count down from 5 to 1 then say "Blastoff!"
+
+console.log("Task 2");
+
+for (i = 5; i = 1; i--) {
+    console.log(i);
+
+    if (i = i) {
+        console.log("Blastoff!")
+    }
+}
+
+// Task 3 - 
+
+console.log("Task 3");
+
+// Task 4 - 
+
+console.log("Task 4");
