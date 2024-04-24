f8c7405a4e1524474b45a3019ba40fe88d971670
ChronosWithA-K <143678373+ChronosWithA-K@users.noreply.github.com>
2024-04-24 23:47:12 +0000

    finish task 4

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,12 @@ for (i = 0; i < 51; i += 2) {
 
 console.log("Task 4");
 
+let sum = 0;
+for (i = 1; i < 101; i++) {
+    sum += i;
+}
 
+console.log(sum)
 
 // Task 5 - Print the first 10 factorials. The factorial of a number n is n multiplied by each integer less than it, down to 1, written as n!
 
