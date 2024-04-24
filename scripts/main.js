e33e347b5c5831c78b0f450e3abcb8105b821fd7
ChronosWithA-K <143678373+ChronosWithA-K@users.noreply.github.com>
2024-04-24 16:51:56 +0000

    rework task 1 solution, finish task 3

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ for (i = 1; i < 11; i * i) {
 
 console.log("Task 2");
 
-for (i = 5; i = 1; i--) {
+for (i = 6; i < 1; i--) {
     console.log(i);
 
     if (i = i) {
@@ -18,10 +18,18 @@ for (i = 5; i = 1; i--) {
     }
 }
 
-// Task 3 - 
+// Task 3 - Print the even numbers from 1 to 50 (including 50)
 
 console.log("Task 3");
 
-// Task 4 - 
+for (i = 0; i < 51; i += 2) {
+    console.log(i);
+}
+
+// Task 4 - Print the sum of 1 through 100.
 
 console.log("Task 4");
+
+// Task 5 - Print the first 10 factorials. The factorial of a number n is n multiplied by each integer less than it, down to 1, written as n!
+
+console.log("Task 5");
