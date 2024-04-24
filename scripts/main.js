9425aed4e2651aec8adb9bc71d6df36a04c18281
ChronosWithA-K <143678373+ChronosWithA-K@users.noreply.github.com>
2024-04-24 23:43:08 +0000

    fix task 1, add link to js tester

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
+// JS Tester - https://playcode.io/javascript
+
 // Task 1 - Print the first 10 square numbers (1*1 = 1, 2*2 = 4, etc.)
 
 console.log("Task 1");
 
-for (i = 1; i < 11; i * i) {
-    console.log(i);
+for (i = 1; i < 11; i++) {
+    console.log(i * i);
 }
 
 // Task 2 - Count down from 5 to 1 then say "Blastoff!"
