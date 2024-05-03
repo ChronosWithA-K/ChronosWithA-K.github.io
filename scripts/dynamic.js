e303b9ebeb323a999d9c5c9b3ba96170ddc8de0f
ChronosWithA-K <143678373+ChronosWithA-K@users.noreply.github.com>
2024-05-03 15:53:56 +0000

    add links to mdn sites on dynamic.js

--- scripts/dynamic.js
+++ scripts/dynamic.js
@@ -1,4 +1,4 @@
-// Task 1 - Create an array containing at least 7 strings picked from this list of CSS color values.
+// Task 1 - Create an array containing at least 7 strings picked from this list of CSS color values (https://developer.mozilla.org/en-US/docs/Web/CSS/named-color).
 
 
 
@@ -8,8 +8,8 @@
 
 // Task 3 - Create a new list item element for each item in the array you created.
 /**
- * Set the innerTextLinks to an external site. property of each element to be the string in the array.
- * Set the colorLinks to an external site. CSS property (not a regular property, but part of the style!) of the element to be the string in the array.
+ * Set the innerText (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText)  property of each element to be the string in the array.
+ * Set the color (https://developer.mozilla.org/en-US/docs/Web/CSS/color) CSS property (not a regular property, but part of the style!) of the element to be the string in the array.
  * Add the element to the end of your unordered list.
  */
 
