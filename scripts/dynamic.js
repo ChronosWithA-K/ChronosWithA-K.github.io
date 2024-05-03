bd23391512fbde04b45dc4a2397734b5f2192cc0
ChronosWithA-K <143678373+ChronosWithA-K@users.noreply.github.com>
2024-05-03 15:52:14 +0000

    add tasks to dynamic.js

---
+++ scripts/dynamic.js
@@ -0,0 +1,20 @@
+// Task 1 - Create an array containing at least 7 strings picked from this list of CSS color values.
+
+
+
+// Task 2 - Create an unordered list element and store it in a variable.
+
+
+
+// Task 3 - Create a new list item element for each item in the array you created.
+/**
+ * Set the innerTextLinks to an external site. property of each element to be the string in the array.
+ * Set the colorLinks to an external site. CSS property (not a regular property, but part of the style!) of the element to be the string in the array.
+ * Add the element to the end of your unordered list.
+ */
+
+
+
+// Task 4 - Add the whole list to the end of the body of your webpage.
+
+
