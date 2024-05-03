53da6d4c0f75c82543a4bc3487ec2c0b0902cd5a
ChronosWithA-K <143678373+ChronosWithA-K@users.noreply.github.com>
2024-05-03 16:31:57 +0000

    finish tasks 3, 4

--- scripts/dynamic.js
+++ scripts/dynamic.js
@@ -2,7 +2,7 @@
 
 console.log("Task 1");
 
-let colourArr = ["black", "red", "teal", "crimson", "darkmagenta", "darkorchid", "linen"]
+let colourArr = ["black", "red", "teal", "crimson", "darkmagenta", "darkorchid", "linen"];
 
 // Task 2 - Create an unordered list element and store it in a variable.
 
@@ -10,15 +10,25 @@ console.log("Task 2");
 
 let unorderedList = document.createElement("ul");
 
-// Task 3 - Create a new list item element for each item in the array you created.
-/**
- * Set the innerText (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText)  property of each element to be the string in the array.
- * Set the color (https://developer.mozilla.org/en-US/docs/Web/CSS/color) CSS property (not a regular property, but part of the style!) of the element to be the string in the array.
- * Add the element to the end of your unordered list.
- */
+// Task 3 - Create a new list item element for each item in the array you created (and do some other things).
 
 console.log("Task 3");
 
+for (i = 0; i < colourArr.length; i++) {
+    let listItemEl = document.createElement("li");
+
+//  Set the innerText (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText) property of each element to be the string in the array.
+    listItemEl.value = source.innerText;
+
+//  Set the color (https://developer.mozilla.org/en-US/docs/Web/CSS/color) CSS property (not a regular property, but part of the style!) of the element to be the string in the array.
+    listItemEl.style.color = colourArr.length[i + 1];
+
+//  Add the element to the end of your unordered list.
+    unorderedList.appendChild(listItemEl);
+}
+
 // Task 4 - Add the whole list to the end of the body of your webpage.
 
-console.log("Task 4");
+    console.log("Task 4");
+    
+    document.body.appendChild(unorderedList);
