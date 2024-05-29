11c94a7e13c81f3ad3602cea0dac2b3918d78fe4
ChronosWithA-K <143678373+ChronosWithA-K@users.noreply.github.com>
2024-05-29 16:50:10 +0000

    add download icon for devlogs page, start devlogs list

---
+++ scripts/devlogs.js
@@ -0,0 +1,12 @@
+const devlogList = document.createElement("ul");
+devlogList.classList.add("cards");
+
+const devlogs = [
+    {
+        name: "Upriver (placeholder)",
+        description: "Play a game where you take control of a salmon, swimming upriver in an attempt to reach the spawn pools (not finalised)",
+        status: "In Development",
+        devlogsIcon: "images/devlog_icon.png",
+        downloadIcon: "images/download_icon.png",
+    }
+]
