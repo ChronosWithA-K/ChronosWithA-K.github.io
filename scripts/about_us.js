9f3e8648ff8ef5e8df62a9e03c325809c990b129
Daemons <143678373+ChronosWithA-K@users.noreply.github.com>
2025-06-02 19:43:40 -0700

    offload club site

--- scripts/about_us.js
+++
@@ -1,6 +0,0 @@
-// Code to add an event listener to the submit button for the Get in Touch form.
-const formBtn = document.querySelector("button"); // Make sure to change if another button tag is added to about-us.html
-
-formBtn.addEventListener("click", function() {
-    formBtn.innerText = "Thanks for submitting the form! We'll try to get in touch as soon as possible."
-});
